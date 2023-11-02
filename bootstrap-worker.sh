53762a32eb819d1527b4e927eee3e4de3545848c
Evardanyan <64473803+Evardanyan@users.noreply.github.com>
2023-11-03 00:40:41 +0400

    Update bootstrap-worker.sh

--- bootstrap-worker.sh
+++ bootstrap-worker.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Install Docker
-sudo apt-get update
-sudo apt-get install -y docker.io
+# Update and install Docker
+sudo apt-get update && sudo apt-get install -y docker.io
 
-# Install kubeadm, kubelet, and kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+# Add the Kubernetes signing key and repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+
+# Update package listings and install kubelet, kubeadm, and kubectl
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+
+# Mark them to hold (prevent automatic update)
+sudo apt-mark hold docker.io kubelet kubeadm kubectl
+
+
